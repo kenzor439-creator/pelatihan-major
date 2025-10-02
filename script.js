1427caca09dc240fd6132d70b8e57cfa7b3ef698
kenzor439-creator <kenzor439@gmail.com>
2025-10-02 12:19:09 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1.pilih tombol berdsarkan id
+const tombolsapa= document.geElementByld('sapaButton')
+// 2. tambahkan event listener 'click'
+tobolsapa.addEventLIstener('click,function() {
+// 3.jalankan kode ketika tombol di klik
+alert('halo! terimakasih sudah berkunjung!');
+});
